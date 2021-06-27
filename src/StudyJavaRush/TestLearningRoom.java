99d59b468332a53737069e8bbc24c994ba5eb9ca
Viento <sterribilis@gmail.com>
2021-06-27 13:54:30 +0300

    Modified version

--- src/StudyJavaRush/TestLearningRoom.java
+++ src/StudyJavaRush/TestLearningRoom.java
@@ -8,53 +8,33 @@ import java.util.*;
 public class TestLearningRoom {
     public static void main(String[] args) throws IOException {
         ArrayList<String> commandCollection = new ArrayList<>();
-        ArrayList<String> listOfKeys = new ArrayList<>();
-        ArrayList<String> listOfValues = new ArrayList<>();
-
         ArrayList<String> names = new ArrayList<>();
-        ArrayList<String> tip = new ArrayList<>();
-        ArrayList<Integer> pricePerKilogram = new ArrayList<>();
-        ArrayList<Integer> kilograms = new ArrayList<>();
-        ArrayList<Integer> price = new ArrayList<>();
-        ArrayList<String> indexes = new ArrayList<>();
+        ArrayList<String> listOfTip = new ArrayList<>();
+
+        ArrayList<Plant> plants = new ArrayList<>();
 
         //список команд для консоли юзверя
         commandCollection.add("help");
         commandCollection.add("sort.natural");
         commandCollection.add("sort.reverse");
         commandCollection.add("print");
-        commandCollection.add("values");
+        commandCollection.add("tip");
         commandCollection.add("end");
         commandCollection.add("name");
         commandCollection.add("setName");
 
-        Plant watermelon = new Plant("арбуз", "ягода", 12, 23, 0);
-        Plant banana = new Plant("банан", "трава", 30, 38, 1);
-        Plant cherry = new Plant("вишня", "ягода", 153, 48, 2);
-        Plant pear = new Plant("груша", "овощь", 412, 37, 3);
-        Plant melon = new Plant("дыня", "овощ", 10, 16, 4);
-        Plant blackberry = new Plant("ежевика", "куст", 912, 10, 5);
-        Plant ginseng = new Plant("женьшень", "корень", 212, 22, 6);
-        Plant strawberry = new Plant("земляника", "ягода", 132, 234, 7);
-        Plant iris = new Plant("ирис", "цветок", 142, 2, 8);
-        Plant potato = new Plant("картофель", "клубень", 212, 3, 9);
-        Plant strawberry2 = new Plant("клубника", "ягода", 127, 223, 10);
-        indexes.add("watermelon");
-        indexes.add("banana");
-        indexes.add("cherry");
-        indexes.add("pear");
-        indexes.add("melon");
-        indexes.add("blackberry");
-        indexes.add("ginseng");
-        indexes.add("strawberry");
-        indexes.add("iris");
-        indexes.add("potato");
-        indexes.add("strawberry2");
+        plants.add(new Plant("арбуз", "ягода", 12, 23));
+        plants.add(new Plant("банан", "трава", 30, 38));
+        plants.add(new Plant("вишня", "ягода", 153, 48));
+        plants.add(new Plant("груша", "овощь", 412, 37));
+        plants.add(new Plant("дыня", "овощ", 10, 16));
+        plants.add(new Plant("ежевика", "куст", 912, 10));
+        plants.add(new Plant("женьшень", "корень", 212, 22));
+        plants.add(new Plant("земляника", "ягода", 132, 234));
+        plants.add(new Plant("ирис", "цветок", 142, 2));
+        plants.add(new Plant("картофель", "клубень", 212, 3));
+        plants.add(new Plant("клубника", "ягода", 127, 22));
 
-        //создаёт список неповторающихся типов
-        for (int i = 0; i < indexes.size(); i++) {
-
-        }
         //выводит уже отфильтрованные значения
         String text;
         Person person = new Person();
@@ -74,29 +54,27 @@ public class TestLearningRoom {
                 System.out.println("Текущее имя = " + person.getName());
                 System.out.println("Для смены имени вы можете использовать команду setName");
             }
-//            if (text.equals(("values"))){
-//                indexes.get();
-//            }
-            /*if (text.equals("values")) {
+            if (text.equals("tip")) {
+                plants.stream().map(Plant::getTip).filter(t -> !listOfTip.contains(t)).forEach(listOfTip::add);
+
                 System.out.println("Выберите значение");
-                for (int i = 0; i < map.size(); i++) {
-                    String next = iteratorOfValues.next();
-                    if (!listOfValues.contains(next)) listOfValues.add(next);
-                    //создаёт список значений без повторов
-                }
-                for (String value : listOfValues) {//выводит список значений
+
+                //создаёт список неповторающихся типов
+                for (String value : listOfTip) {//выводит список значений без повторов
                     System.out.println(value);
                 }
-                String selectedValue = readText();//выбраное значение
-                for (int i = 0; i < map.size(); i++) {
-                    //сравнение значения в списке с введённым значением
-                    //сортировка именно этого значения
-                    //добавление элемента в список ключей
-                    // при условии отсутствия данного элемента в данном списке
-                    if (iteratorOfValues1.next().equals(selectedValue)) listOfKeys.add(iteratorOfKeys.next());
-                    else iteratorOfKeys.next();
+                String selectedTip = readText();//выбраное значение
+                //сравнение значения в списке с введённым значением
+                //сортировка именно этого значения
+                //добавление элемента в список имён
+                // при условии отсутствия данного имени в данном списке
+                for (Plant plant : plants) {
+                    if (plant.getTip().equals(selectedTip)) {
+                        String name = plant.getName();
+                        names.add(name);
+                    }
                 }
-            }*/
+            }
             if (text.equals("end")) break;
             if (text.equals("help")) help(commandCollection);
             if (text.equals("sort")) {
@@ -104,9 +82,9 @@ public class TestLearningRoom {
                 System.out.println("sort.natural -По возрастанию");
                 System.out.println("sort.reverse -По возрастанию");
             }
-            if (text.equals("sort.natural")) reverser(text, listOfKeys);
-            if (text.equals("sort.reverse")) reverser(text, listOfKeys);
-            if (text.equals("print")) listOfKeys.forEach(System.out::println);
+            if (text.equals("sort.natural")) reverser(text, names);
+            if (text.equals("sort.reverse")) reverser(text, names);
+            if (text.equals("print")) names.forEach(System.out::println);
         }
     }
 
@@ -147,21 +125,15 @@ class Plant {
     private int pricePerKilogram;
     private int kilograms;
     private int price = pricePerKilogram * kilograms;
-    private int index;
 
 //    public void getInfo() {
 //    }
 
-    public Plant(String name, String tip, int pricePerKilogram, int kilograms, int index) {
+    public Plant(String name, String tip, int pricePerKilogram, int kilograms) {
         this.name = name;
         this.tip = tip;
         this.pricePerKilogram = pricePerKilogram;
         this.kilograms = kilograms;
-        this.index = index;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
     }
 
     public String getTip() {
