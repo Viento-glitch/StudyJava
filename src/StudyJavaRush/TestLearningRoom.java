0f59a15b56b6209a3133dd40c0f46f5bf513378e
Viento <sterribilis@gmail.com>
2021-06-27 12:05:25 +0300

    Reworking concepcion (OOP?)

--- src/StudyJavaRush/TestLearningRoom.java
+++ src/StudyJavaRush/TestLearningRoom.java
@@ -5,12 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class MapAndSortCollections {
+public class TestLearningRoom {
     public static void main(String[] args) throws IOException {
-        Map<String, String> map = new HashMap<>();
         ArrayList<String> commandCollection = new ArrayList<>();
         ArrayList<String> listOfKeys = new ArrayList<>();
         ArrayList<String> listOfValues = new ArrayList<>();
+
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> tip = new ArrayList<>();
+        ArrayList<Integer> pricePerKilogram = new ArrayList<>();
+        ArrayList<Integer> kilograms = new ArrayList<>();
+        ArrayList<Integer> price = new ArrayList<>();
+        ArrayList<String> indexes = new ArrayList<>();
 
         //список команд для консоли юзверя
         commandCollection.add("help");
@@ -22,19 +28,33 @@ public class MapAndSortCollections {
         commandCollection.add("name");
         commandCollection.add("setName");
 
-        map.put("арбуз", "ягода");
-        map.put("банан", "трава");
-        map.put("вишня", "ягода");
-        map.put("груша", "фрукт");
-        map.put("дыня", "овощ");
-        map.put("ежевика", "куст");
-        map.put("жень-шень", "корень");
-        map.put("земляника", "ягода");
-        map.put("ирис", "цветок");
-        map.put("картофель", "клубень");
-        map.put("клубника", "ягода");
+        Plant watermelon = new Plant("арбуз", "ягода", 12, 23, 0);
+        Plant banana = new Plant("банан", "трава", 30, 38, 1);
+        Plant cherry = new Plant("вишня", "ягода", 153, 48, 2);
+        Plant pear = new Plant("груша", "овощь", 412, 37, 3);
+        Plant melon = new Plant("дыня", "овощ", 10, 16, 4);
+        Plant blackberry = new Plant("ежевика", "куст", 912, 10, 5);
+        Plant ginseng = new Plant("женьшень", "корень", 212, 22, 6);
+        Plant strawberry = new Plant("земляника", "ягода", 132, 234, 7);
+        Plant iris = new Plant("ирис", "цветок", 142, 2, 8);
+        Plant potato = new Plant("картофель", "клубень", 212, 3, 9);
+        Plant strawberry2 = new Plant("клубника", "ягода", 127, 223, 10);
+        indexes.add("watermelon");
+        indexes.add("banana");
+        indexes.add("cherry");
+        indexes.add("pear");
+        indexes.add("melon");
+        indexes.add("blackberry");
+        indexes.add("ginseng");
+        indexes.add("strawberry");
+        indexes.add("iris");
+        indexes.add("potato");
+        indexes.add("strawberry2");
 
+        //создаёт список неповторающихся типов
+        for (int i = 0; i < indexes.size(); i++) {
 
+        }
         //выводит уже отфильтрованные значения
         String text;
         Person person = new Person();
@@ -54,10 +74,10 @@ public class MapAndSortCollections {
                 System.out.println("Текущее имя = " + person.getName());
                 System.out.println("Для смены имени вы можете использовать команду setName");
             }
-            if (text.equals("values")) {
-                Iterator<String> iteratorOfValues = map.values().iterator();
-                Iterator<String> iteratorOfValues1 = map.values().iterator();
-                Iterator<String> iteratorOfKeys = map.keySet().iterator();
+//            if (text.equals(("values"))){
+//                indexes.get();
+//            }
+            /*if (text.equals("values")) {
                 System.out.println("Выберите значение");
                 for (int i = 0; i < map.size(); i++) {
                     String next = iteratorOfValues.next();
@@ -76,7 +96,7 @@ public class MapAndSortCollections {
                     if (iteratorOfValues1.next().equals(selectedValue)) listOfKeys.add(iteratorOfKeys.next());
                     else iteratorOfKeys.next();
                 }
-            }
+            }*/
             if (text.equals("end")) break;
             if (text.equals("help")) help(commandCollection);
             if (text.equals("sort")) {
@@ -119,4 +139,66 @@ class Person {
     public void setName(String name) {
         this.name = name;
     }
+}
+
+class Plant {
+    private String name;
+    private String tip;
+    private int pricePerKilogram;
+    private int kilograms;
+    private int price = pricePerKilogram * kilograms;
+    private int index;
+
+//    public void getInfo() {
+//    }
+
+    public Plant(String name, String tip, int pricePerKilogram, int kilograms, int index) {
+        this.name = name;
+        this.tip = tip;
+        this.pricePerKilogram = pricePerKilogram;
+        this.kilograms = kilograms;
+        this.index = index;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getTip() {
+        return tip;
+    }
+
+    public void setTip(String tip) {
+        this.tip = tip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPricePerKilogram() {
+        return pricePerKilogram;
+    }
+
+    public void setPricePerKilogram(int pricePerKilogram) {
+        this.pricePerKilogram = pricePerKilogram;
+    }
+
+    public int getKilograms() {
+        return kilograms;
+    }
+
+    public void setKilograms(int kilograms) {
+        this.kilograms = kilograms;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
 }
